82a9a2bbf0fdbec47a84c79034f299d311c63b6a
Mahmud <mahxgithub@gmail.com>
2026-08-25 22:36:07 +0100

    Improved the code of the layout page file

--- inventry_frontend/src/app/(dashboard)/page.tsx
+++ inventry_frontend/src/app/(dashboard)/page.tsx
@@ -1,4 +1,4 @@
-import { AppSidebar } from "@/components/app-sidebar"
+
 import { ChartAreaInteractive } from "@/components/chart-area-interactive"
 import { DataTable } from "@/components/data-table"
 import { SectionCards } from "@/components/section-cards"
@@ -9,15 +9,7 @@ import data from "./data.json"
 
 export default function Page() {
   return (
-    <SidebarProvider
-      style={
-        {
-          "--sidebar-width": "calc(var(--spacing) * 72)",
-          "--header-height": "calc(var(--spacing) * 12)",
-        } as React.CSSProperties
-      }
-    >
-      <AppSidebar variant="inset" />
+    
       <SidebarInset>
         <SiteHeader title="Dashboard" />
         <div className="flex flex-1 flex-col">
@@ -32,6 +24,5 @@ export default function Page() {
           </div>
         </div>
       </SidebarInset>
-    </SidebarProvider>
   )
 }
--- inventry_frontend/src/app/layout.tsx
+++ inventry_frontend/src/app/layout.tsx
@@ -2,10 +2,15 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans, Public_Sans, Noto_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 
-const notoSansHeading = Noto_Sans({subsets:['latin'],variable:'--font-heading'});
+const notoSansHeading = Noto_Sans({
+  subsets: ["latin"],
+  variable: "--font-heading",
+});
 
-const publicSans = Public_Sans({subsets:['latin'],variable:'--font-sans'});
+const publicSans = Public_Sans({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Inventry - Modern Inventory Management",
@@ -18,9 +23,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={cn("h-full", "antialiased", "font-sans", publicSans.variable, notoSansHeading.variable)}>
+    <html
+      lang="en"
+      className={cn(
+        "h-full",
+        "antialiased",
+        "font-sans",
+        publicSans.variable,
+        notoSansHeading.variable,
+      )}
+    >
       <body className="min-h-full bg-[#F6F8FC] text-slate-800 antialiased selection:bg-indigo-500 selection:text-white">
-        {children}
+        <SidebarProvider
+          style={
+            {
+              "--sidebar-width": "calc(var(--spacing) * 72)",
+              "--header-height": "calc(var(--spacing) * 12)",
+            } as React.CSSProperties
+          }
+        >
+          <AppSidebar variant="inset" />
+          {children}
+        </SidebarProvider>
       </body>
     </html>
   );
