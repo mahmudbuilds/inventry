982192105d2caac033372e46de829f6c21619b53
Mahmud <mahxgithub@gmail.com>
2026-09-04 11:55:03 +0100

    chore: safe migrations in build.sh and db fallback in settings

--- inventry_backend/inventry_backend/settings.py
+++ inventry_backend/inventry_backend/settings.py
@@ -107,12 +107,21 @@ WSGI_APPLICATION = 'inventry_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
-    )
-}
+database_url = os.environ.get('DATABASE_URL')
+if database_url:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=database_url,
+            conn_max_age=600,
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
